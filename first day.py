1d575b68875ae6c92e39387d787bae287b7e394a
Deva677 <143687327+Deva677@users.noreply.github.com>
2023-12-21 17:40:40 +0530

    Add files via upload

---
+++ first day.py
@@ -0,0 +1,33 @@
+
+#integers or numbers
+age=30
+print("the age of:",(type(age)),"the address of age:",(id(age)))
+Age=age+30
+print(Age)
+print("the updated age of:",(type(Age)),"the adress of updated age:",(id(Age)))
+#flaot
+age=30.8
+print("the age of:",(type(age)),"the address of age:",(id(age)))
+#complex
+age=30.8j
+print("the age of:",(type(age)),"the address of age:",(id(age)))
+#sring
+name="deva"
+print("the name of:",(type(name)),"the address of name:",(id(name)))
+Name="Deva"
+print("the name of:",(type(Name)),"the adress of name:",(id(Name)))
+_name="deva"
+print("the name of:",(type(name)),"the adress of name:",(id(name)))
+#boolen
+value=True
+print(" the value of:",(type(value))," the adress of name:",(id(value)))
+Value=False
+print(" the value of:",(type(value))," the adress of name:",(id(value)))
+
+
+
+
+
+
+
+
