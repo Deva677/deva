f70d6d43f620560670993aff91c6151b091c4d0c
Deva677 <143687327+Deva677@users.noreply.github.com>
2023-12-21 17:43:58 +0530

    Add files via upload

---
+++ pratice day 1.py
@@ -0,0 +1,20 @@
+# num1=40
+# num2=50
+# result=num1+num2
+# print("the result of two number:",result)
+# print(result)
+# Name="deva"
+# print(Name)
+# FatherN="swamy"
+# print(FatherN)
+# aadharnum=7199554
+# print(aadharnum)
+# radius=(int(input("enter the radius of the circle(only enter integers")))
+# result=3.142*radius*radius
+# print(result)
+#rectangle
+length=(int(input("enter the length of the rectangle(only integers)")))
+width=(int(input("enter the width of the rectangle(only integers)")))
+area=length*width
+print(area)
+
