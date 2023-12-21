0ffc44abaed273b641e1aedf8566acd3b23cece2
Deva677 <143687327+Deva677@users.noreply.github.com>
2023-12-21 17:45:13 +0530

    Add files via upload

---
+++ operators.py
@@ -0,0 +1,140 @@
+# Arithmetic operators
+num1=40
+num2=50
+num3=num1+num2
+print("the addition of two numbers",num3)
+num3=num2-num1
+print("the subtraction of two numbers",num3)
+num3=num1*num2
+print("the multiplication of two numbers",num3)
+num3=num1/num2
+print("the divsion of two numbers",num3)
+num3=num1//num2
+print("the floor of two numbers",num3)
+num3=num1%num2
+print("the remainder of two numbers",num3)
+num3=num1**3
+print("the exponet of two numbers",num3)
+
+# float
+deva=10.3
+chandu=30.5
+sum=deva+chandu
+print("the addtion of two numbers",sum,type(sum))
+sum=deva-chandu
+print("the subtraction of two numbers",sum,type(sum))
+sum=deva*chandu
+print("the multiplication of two numbers",sum,type(sum))
+sum=deva/chandu
+print("the division of two numbers",sum,type(sum))
+sum=deva//chandu
+print("the floor of two numbers",sum,type(sum))
+sum=deva%chandu
+print("the remainder of two numbers",sum,type(sum))
+sum=deva**chandu
+print("the exponet of two numbers",sum,type(sum))
+
+ # complex
+deva=10.3j
+chandu=30.5j
+sum=deva+chandu
+print("the addtion of two numbers",sum,type(sum))
+sum=deva-chandu
+print("the subtraction of two numbers",sum,type(sum))
+sum=deva*chandu
+print("the multiplication of two numbers",sum,type(sum))
+sum=deva/chandu
+print("the division of two numbers",sum,type(sum))
+# sum=deva//chandu
+# print("the floor of two numbers",sum,type(sum))
+# sum=deva%chandu
+# print("the remainder of two numbers",sum,type(sum))
+sum=deva**chandu
+print("the exponet of two numbers",sum,type(sum))
+
+#assigment operators
+x=4
+x+=6
+print("the addtion of two numbers",x, type(x),id(x))
+
+x=4
+x-=6
+print("the subtraction of two numbers",x, type(x),id(x))
+
+x=4
+x*=6
+print("the multiplicationof two numbers",x, type(x),id(x))
+
+x=4
+x/=6
+print("the division of two numbers",x, type(x),id(x))
+
+x=4
+x//=6
+print("the floor of two numbers",x, type(x),id(x))
+
+x=4
+x%=6
+print("the remainder of two numbers",x, type(x),id(x))
+
+x=4
+x**=6
+print("the exponet of two numbers",x, type(x),id(x))
+
+# float
+x=5.01
+x+=3.02
+print("the addtion of two numbers",x, type(x),id(x))
+
+x=5.01
+x-=3.02
+print("the subtraction of two numbers",x, type(x),id(x))
+
+x=5.01
+x*=3.02
+print("the multiplication of two numbers",x, type(x),id(x))
+
+x=5.01
+x/=3.02
+print("the division of two numbers",x, type(x),id(x))
+
+x=5.01
+x//=3.02
+print("the floor of two numbers",x, type(x),id(x))
+
+x=5.01
+x%=3.02
+print("the remainder of two numbers",x, type(x),id(x))
+
+x=5.01
+x**=3.02
+print("the exponet of two numbers",x, type(x),id(x))
+
+# complex
+x=5.01j
+x+=3.02
+print("the addtion of two numbers",x, type(x),id(x))
+
+x=5.01j
+x-=3.02
+print("the subtraction of two numbers",x, type(x),id(x))
+
+x=5.01j
+x*=3.02
+print("the multiplication of two numbers",x, type(x),id(x))
+
+x=5.01j
+x/=3.02
+print("the division of two numbers",x, type(x),id(x))
+
+# x=5.01j
+# x//=3.02
+# print("the floor of two numbers",x, type(x),id(x))
+#
+# x=5.01j
+# x%=3.02
+# print("the remainder of two numbers",x, type(x),id(x))
+
+x=5.01j
+x**=3.02
+print("the exponent of two numbers",x, type(x),id(x))
