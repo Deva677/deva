ebb06bcedadc296ab6a09cb4632552485276fbac
Deva677 <143687327+Deva677@users.noreply.github.com>
2023-12-29 17:42:07 +0530

    Add files via upload

---
+++ second day.py
@@ -0,0 +1,156 @@
+#int to bool
+salary=10000
+iscredited=False
+bsalary=bool(salary)
+print("the salary credited:",iscredited,"check",type(salary))
+updatedsalary=iscredited+1000
+print(updatedsalary)
+bupdatedsalary=bool(updatedsalary)
+print("the updated salary:",iscredited,"check",bool(updatedsalary),"thetype of updatedsalary:",type(bupdatedsalary))
+print("it is a downcasting")
+#int to float
+salary=10000
+bsalary=float(salary)
+print("the salary is ", iscredited,"check",float(salary))
+updatedsalary=bsalary+1000
+print("the updated salary:",iscredited,"check",float(updatedsalary),"thetype of updatedsalary:",type(updatedsalary))
+print("it is  upcasting")
+#int to string
+salary=10000
+iscredited=False
+csalary=str(salary)
+print("the salary is ", iscredited,"check",str(salary),type(salary))
+updatedsalary=csalary
+print(updatedsalary)
+cupdatedsalary=str(updatedsalary)
+print("the updated salary:",iscredited,"check",str(cupdatedsalary),"thetype of updatedsalary:",type(cupdatedsalary))
+print("it is  upcasting")
+#float to int
+salary=1000.1
+csalary=int(salary)
+print("the salary is ", iscredited,"check",int(salary),type(salary))
+updatedsalary=csalary
+print(updatedsalary)
+cupdatedsalary=int(updatedsalary)
+print("the updated salary:",iscredited,"check",int(cupdatedsalary),"thetype of updatedsalary:",type(cupdatedsalary))
+print("it is  downcasting")
+#float to bool
+salary=1000.1
+csalary=bool(salary)
+print("the salary is ", iscredited,"check",bool(salary),type(salary))
+updatedsalary=csalary
+print(updatedsalary)
+cupdatedsalary=bool(updatedsalary)
+print("the updated salary:",iscredited,"check",bool(cupdatedsalary),"thetype of updatedsalary:",type(cupdatedsalary))
+print("it is  downcasting")
+#float to str
+salary=1000.1
+csalary=str(salary)
+print("the salary is ", iscredited,"check",str(salary),type(salary))
+updatedsalary=csalary
+print(updatedsalary)
+cupdatedsalary=str(updatedsalary)
+print("the updated salary:",iscredited,"check",str(cupdatedsalary),"thetype of updatedsalary:",type(cupdatedsalary))
+print("it is  upcasting")
+# str to int
+salary='10000'
+csalary=int(salary)
+print("the salary is ", iscredited,"check",int(salary),type(salary))
+updatedsalary=csalary
+print(updatedsalary)
+cupdatedsalary=int(updatedsalary)
+print("the updated salary:",iscredited,"check",int(cupdatedsalary),"thetype of updatedsalary:",type(cupdatedsalary))
+print("it is  upcasting")
+#str to float
+salary='10000'
+csalary=float(salary)
+print("the salary is ", iscredited,"check",float(salary),type(salary))
+updatedsalary=csalary
+print(updatedsalary)
+cupdatedsalary=float(updatedsalary)
+print("the updated salary:",iscredited,"check",float(cupdatedsalary),"thetype of updatedsalary:",type(cupdatedsalary))
+print("it is  upcasting")
+# str to bool
+salary='10000'
+csalary=bool(salary)
+print("the salary is ", iscredited,"check",bool(salary),type(salary))
+updatedsalary=csalary
+print(updatedsalary)
+cupdatedsalary=bool(updatedsalary)
+print("the updated salary:",iscredited,"check",bool(cupdatedsalary),"thetype of updatedsalary:",type(cupdatedsalary))
+print("it is  upcasting")
+#bool to int
+salary=True
+iscredited=False
+bsalary=int(salary)
+print("the salary credited:",iscredited,"check",bool(salary))
+print(updatedsalary)
+bupdatedsalary=int(updatedsalary)
+print("the updated salary:",iscredited,"check",int(updatedsalary),"thetype of updatedsalary:",type(bupdatedsalary))
+print("it is a upcasting")
+#bool to float
+salary=True
+iscredited=False
+bsalary=float(salary)
+print("the salary credited:",iscredited,"check",float(salary))
+print(updatedsalary)
+bupdatedsalary=float(updatedsalary)
+print("the updated salary:",iscredited,"check",float(updatedsalary),"thetype of updatedsalary:",type(bupdatedsalary))
+print("it is a upcasting")
+# bool to str
+salary=True
+iscredited=False
+bsalary=str(salary)
+print("the salary credited:",iscredited,"check",str(salary))
+print(updatedsalary)
+bupdatedsalary=str(updatedsalary)
+print("the updated salary:",iscredited,"check",str(updatedsalary),"thetype of updatedsalary:",type(bupdatedsalary))
+print("it is a upcasting")
+#complex to str
+salary=34.4j
+csalary=str(salary)
+print("the salary is ", iscredited,"check",str(salary),type(salary))
+updatedsalary=csalary
+print(updatedsalary)
+cupdatedsalary=str(updatedsalary)
+print("the updated salary:",iscredited,"check",str(cupdatedsalary),"thetype of updatedsalary:",type(cupdatedsalary))
+print("it is  upcasting")
+#complex to float
+# salary=34.4j
+# csalary=float(salary)
+# print("the salary is ", iscredited,"check",float(salary),type(salary))
+# updatedsalary=csalary
+# print(updatedsalary)
+# cupdatedsalary=float(updatedsalary)
+# print("the updated salary:",iscredited,"check",float(cupdatedsalary),"thetype of updatedsalary:",type(cupdatedsalary))
+# print("it is  upcasting")
+#complex to bool
+salary=34.4j
+csalary=bool(salary)
+print("the salary is ", iscredited,"check",bool(salary),type(salary))
+updatedsalary=csalary
+print(updatedsalary)
+cupdatedsalary=bool(updatedsalary)
+print("the updated salary:",iscredited,"check",bool(cupdatedsalary),"thetype of updatedsalary:",type(cupdatedsalary))
+print("it is  upcasting")
+# #complex to int
+# salary=34.4j
+# csalary=int(salary)
+# print("the salary is ", iscredited,"check",int(salary),type(salary))
+# updatedsalary=csalary
+# print(updatedsalary)
+# cupdatedsalary=int(updatedsalary)
+# print("the updated salary:",iscredited,"check",int(cupdatedsalary),"thetype of updatedsalary:",type(cupdatedsalary))
+# print("it is  upcasting")
+
+
+
+
+
+
+
+
+
+
+
+
