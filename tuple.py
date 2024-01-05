60ac3e17e3e2c3e118e1c721c0f6be2a3b1b2e0e
Deva677 <143687327+Deva677@users.noreply.github.com>
2024-01-05 18:04:23 +0530

    Add files via upload

---
+++ tuple.py
@@ -0,0 +1,52 @@
+
+# add set
+myset={1,2,34,45,5,67,3,2,1,}
+myset.add("deva")
+print(myset)
+# remove
+myset={"deva","bharath","arun","sree","mani"}
+myset.remove("bharath")
+print(myset)
+# discard
+set={"mani","bharath","mani","Arun","sree","sree"}
+myset.discard("sree")
+print(set)
+# pop
+name={"deva","bharath","arun","sree","mani"}
+name.pop()
+print(name)
+# clear
+name={"deva","bharath","arun","sree","mani"}
+name.clear()
+print(name)
+
+# dictionary
+emptyDict=dict()
+print(emptyDict)
+# items
+myDict={"persons":30,"vegetables":40,"fruits":50}
+print("the items of dict",myDict)
+# keys
+myDict={"persons":30,"vegetables":40,"fruits":50}
+print("the print of keys",myDict.keys())
+# values
+myDict={"persons":30,"vegetables":40,"fruits":50}
+print("the print of keys",myDict.values())
+# items
+myDict={"persons":30,"vegetables":40,"fruits":50}
+print("the print of keys",myDict.items())
+# length
+myDict={"persons":30,"vegetables":40,"fruits":50}
+print(len(myDict))
+# type
+myDict={"persons":30,"vegetables":40,"fruits":50}
+print(type(myDict))
+# add
+myDict={"persons":30,"vegetables":40,"fruits":50}
+myDict.pop(1)
+print(myDict)
+
+
+
+
+
