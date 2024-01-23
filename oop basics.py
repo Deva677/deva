61fd92f302cf249c9322f728d498dced44f06de4
Deva677 <143687327+Deva677@users.noreply.github.com>
2024-01-23 20:07:02 +0530

    23/1/2024
    oops

---
+++ oop basics.py
@@ -0,0 +1,29 @@
+# class Mobile:
+#     def __init__(self,brand,ram,price):
+#         self.brand=brand
+#         self.ram=ram
+#         self.price=price
+#     def display(self):
+#         print("brand:",self.brand)
+#         print("ram:",self.ram)
+#         print("price:",self.price)
+# # for i in range(5):
+# obj=Mobile("apple","8gb",'400000')
+# obj.display()
+# obj=Mobile("onefles","10gb",'30000')
+# obj.display()
+
+class mobile:
+    def __init__(
+
+
+
+
+self):
+        print("this is init")
+    def dispaly(self):
+        print("this is method")
+obj=mobile()
+obj.display()
+
+
