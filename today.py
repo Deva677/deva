337b16d954446c4c97d86482e0315e3b0d07250b
Deva677 <143687327+Deva677@users.noreply.github.com>
2024-02-07 18:04:39 +0530

    7/2/2024
    pratice

---
+++ today.py
@@ -0,0 +1,52 @@
+# class ram():
+#     def fun(self,str):
+#         self.dev=str
+#         print(str)
+# class dev():
+# def fun(self,int):
+#         self.int=int
+#         print(int)
+# obj=ram()
+# obj.fun("deva")
+# obj.fun(40)
+
+
+#
+# class Deva():
+#     def fun1(self,int):
+#         self.int = int
+#         print(int)
+# obj = Deva()
+# obj.fun1(12)
+#
+# class nandhu():
+#     def fun1(self,str,str1):
+#         self.int = str
+#         self.str = str1
+#         print(str+str1)
+# obj = nandhu()
+# obj.fun1("deva","nandhu")
+#
+# class amma():
+#     def fun1(self,float,float1,float2):
+#         self.float = float
+#         self.float1= float1
+#         self.float= float2
+#
+#         print(float,float1,float2)
+# obj = amma()
+#
+# obj.fun1(12.5,34.4,23.5)
+
+def add(a,b,c=0,d=0,e=0):
+    ans=a+b+c+d+e
+    print("Answer:",ans)
+add(2,3)
+add(2,3,4,)
+add(2,3,4,5,)
+add(2,3,4,5,6)
+
+
+
+
+
