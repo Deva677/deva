9d709d1bbda32c34ffc22662e3b60cfe988ebf8e
Deva677 <143687327+Deva677@users.noreply.github.com>
2024-02-23 18:02:15 +0530

    23/2/2024
    datatypes practice

---
+++ feb22.py
@@ -0,0 +1,30 @@
+# list=[1,2,3,4,5,6,7]
+# print(type(list))
+#
+# mylist=(1,2,3,4,5)
+# print(type(mylist))
+#
+# mylist={1,2,3,4,5}
+# print(type(mylist))
+#
+# mylist={"name":"deva","age":23}
+# print(type(mylist))
+# print(mylist)
+
+mylist=[1,2,3,"deva"]
+mylist.append(2)
+print("append value:",mylist)
+mylist.pop(2)
+print("pop value:",mylist)
+mylist.insert(0,2)
+print("copy value:",mylist)
+# mylist.extend(0,)
+# print("extend value:",mylist)
+mylist.remove(2)
+print("remove value:",mylist)
+mylist.reverse()
+print("reverse value:",mylist)
+
+
+
+
